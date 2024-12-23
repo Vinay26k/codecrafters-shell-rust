be28651c853b13cf13523d7b74c73fe4fead0cf3
Vinay Narayana <narayanavinaykumar@gmail.com>
2024-12-23 01:56:05 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 use std::io::{self, Write};
 use std::process;
 
+fn echo(st: &str) {
+    println!("{}", st);
+    io::stdout().flush().unwrap();
+}
+
 fn main() {
     loop {
         // Uncomment this block to pass the first stage
@@ -13,9 +18,14 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        match input.trim() {
-            "exit 0" => process::exit(0),
-            _ => println!("{}: command not found", input.trim()),
+        let mut parts = input.trim().splitn(2, ' ');
+        let cmd = parts.next().unwrap_or("");
+        let args = parts.next().unwrap_or("");
+
+        match (cmd, args) {
+            ("exit", "0") => process::exit(args.parse().unwrap_or(0)),
+            ("echo", args) => echo(args),
+            _ => println!("{}: command not found", cmd),
         }
     }
 }
