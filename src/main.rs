c380002cc1125fc396a0671c120a56f7fb39a549
Vinay Narayana <narayanavinaykumar@gmail.com>
2024-12-23 09:16:22 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process;
+use std::{env, fs, process};
 
 fn echo(st: &str) {
     println!("{}", st);
@@ -23,13 +24,32 @@ fn main() {
         let args = parts.next().unwrap_or("");
 
         let commands = ["exit", "echo", "type"];
+        let mut all_commands: HashMap<String, String> = HashMap::new();
 
+        for each_path in env::var("PATH").unwrap().split(":") {
+            for cmd in fs::read_dir(each_path).unwrap() {
+                if let Ok(cmd_str) = cmd {
+                    let file_name = cmd_str.file_name().into_string().unwrap();
+                    let file_path = fs::canonicalize(cmd_str.path())
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+                    all_commands.insert(file_name, file_path);
+                }
+            }
+        }
+
+        for cmd in commands {
+            all_commands.insert(cmd.to_string(), String::from("a shell builtin"));
+        }
         match (cmd, args) {
             ("exit", "0") => process::exit(args.parse().unwrap()),
             ("echo", args) => echo(args),
             ("type", args) => {
-                if commands.contains(&args) {
-                    println!("{} is a shell builtin", &args)
+                if all_commands.contains_key(&args.to_string()) {
+                    let (k, v) = all_commands.get_key_value(args).unwrap();
+                    println!("{} is {}", k, v)
                 } else {
                     println!("{}: not found", &args)
                 }
