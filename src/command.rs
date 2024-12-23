de5f4c800865088e3ff5c5208c2c7880c5537007
Vinay Narayana <narayanavinaykumar@gmail.com>
2024-12-23 18:10:52 +0100

    codecrafters submit [skip ci]

---
+++ src/command.rs
@@ -0,0 +1,112 @@
+use std::{collections::HashMap, env, fs, process};
+
+pub enum Command {
+    Exit(u8),
+    Echo(String),
+    Pwd,
+    Cd(String),
+    Type(String, HashMap<String, String>),
+    OsExistingCmd(String, String, HashMap<String, String>),
+}
+
+impl Command {
+    pub fn parse(command: &str, args: String, registry: HashMap<String, String>) -> Self {
+        // create a command on parsing the input
+        match command {
+            "exit" => Command::Exit(args.parse::<u8>().unwrap()),
+            "echo" => Command::Echo(args),
+            "pwd" => Command::Pwd,
+            "cd" => Command::Cd(args),
+            "type" => Command::Type(args.to_string(), registry),
+            _ => Command::OsExistingCmd(command.to_string(), args.to_string(), registry),
+        }
+    }
+
+    pub fn execute(&self) {
+        // executes instructions based on command
+        match self {
+            Command::Exit(0) => process::exit(0),
+            Command::Exit(_code) => (),
+            Command::Echo(message) => println!("{}", message),
+            Command::Pwd => {
+                if let Ok(curr_dir) = env::current_dir() {
+                    println!("{}", curr_dir.to_str().unwrap())
+                }
+            }
+            Command::Cd(path) => {
+                let normalized_path = if path.starts_with("~") {
+                    path.replacen("~", env::var("HOME").unwrap_or_default().as_str(), 1)
+                } else if path.starts_with("./") {
+                    path.replacen("./", env::current_dir().unwrap().to_str().unwrap(), 1)
+                } else if path.starts_with("../") {
+                    path.replacen("../", env::current_dir().unwrap().to_str().unwrap(), 1)
+                } else {
+                    path.to_string()
+                };
+                let ch_cmd = env::set_current_dir(normalized_path);
+                match ch_cmd {
+                    Ok(_) => (),
+                    Err(_) => println!("cd: {}: No such file or directory", path),
+                }
+            }
+            Command::Type(cmd, registry) => match cmd.as_str() {
+                "echo" | "exit" | "pwd" | "type" => println!("{} is a shell builtin", cmd),
+                _ => {
+                    if registry.contains_key(cmd) {
+                        let (program, program_path) = registry.get_key_value(cmd).unwrap();
+                        println!("{} is {}", program, program_path)
+                    } else {
+                        println!("{}: not found", cmd)
+                    }
+                }
+            },
+            Command::OsExistingCmd(cmd, args, registry) => {
+                // this leverages registry of commands
+                if registry.contains_key(cmd) {
+                    let (_program, program_path) = registry.get_key_value(cmd).unwrap();
+                    let command_exec = process::Command::new(program_path)
+                        .arg(args)
+                        .stdout(process::Stdio::piped())
+                        .stderr(process::Stdio::piped())
+                        .output()
+                        .unwrap();
+                    print!("{}", String::from_utf8_lossy(&command_exec.stdout));
+                } else {
+                    println!("{}: command not found", cmd);
+                }
+            }
+        }
+    }
+}
+
+pub struct CommandCache {
+    pub registry: HashMap<String, String>,
+}
+
+impl CommandCache {
+    pub fn new() -> Self {
+        let registry: HashMap<String, String> = HashMap::new();
+        Self { registry }
+    }
+
+    fn add(&mut self, cmd: String, cmd_path: String) {
+        self.registry.insert(cmd, cmd_path);
+    }
+
+    pub fn add_from_env_var(&mut self, env_var: &str) {
+        for each_path in env::var(env_var).unwrap_or_default().split(":") {
+            for command in fs::read_dir(each_path).unwrap() {
+                if let Ok(cmd) = command {
+                    self.add(
+                        cmd.file_name().into_string().unwrap(),
+                        fs::canonicalize(cmd.path())
+                            .unwrap()
+                            .to_str()
+                            .unwrap()
+                            .to_string(),
+                    );
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use std::collections::HashMap;
 use std::io::{self, Write};
-use std::{env, fs, process};
 
-fn echo(st: &str) {
-    println!("{}", st);
-    io::stdout().flush().unwrap();
-}
-
-fn pwd() {
-    if let Ok(cur_dir) = env::current_dir() {
-        println!("{}", cur_dir.to_str().unwrap());
-    }
-}
+// local modules
+mod command;
+use command::{Command, CommandCache};
 
 fn main() {
     loop {
@@ -26,54 +17,12 @@ fn main() {
 
         let mut parts = input.trim().splitn(2, ' ');
         let cmd = parts.next().unwrap_or("");
-        let args = parts.next().unwrap_or("");
+        let args = parts.next().unwrap_or("").to_string();
 
-        let commands = ["exit", "echo", "pwd", "type"];
-        let mut all_commands: HashMap<String, String> = HashMap::new();
+        let mut command_registry = CommandCache::new();
+        command_registry.add_from_env_var("PATH");
 
-        for each_path in env::var("PATH").unwrap().split(":") {
-            for cmd in fs::read_dir(each_path).unwrap() {
-                if let Ok(cmd_str) = cmd {
-                    let file_name = cmd_str.file_name().into_string().unwrap();
-                    let file_path = fs::canonicalize(cmd_str.path())
-                        .unwrap()
-                        .to_str()
-                        .unwrap()
-                        .to_string();
-                    all_commands.insert(file_name, file_path);
-                }
-            }
-        }
-
-        for cmd in commands {
-            all_commands.insert(cmd.to_string(), String::from("a shell builtin"));
-        }
-        match (cmd, args) {
-            ("exit", "0") => process::exit(args.parse().unwrap()),
-            ("echo", args) => echo(args),
-            ("pwd", "") => pwd(),
-            ("type", args) => {
-                if all_commands.contains_key(&args.to_string()) {
-                    let (program, path) = all_commands.get_key_value(args).unwrap();
-                    println!("{} is {}", program, path)
-                } else {
-                    println!("{}: not found", &args)
-                }
-            }
-            _ => {
-                if all_commands.contains_key(&cmd.to_string()) {
-                    let (_program, cmd_path) = all_commands.get_key_value(cmd).unwrap();
-                    let output = process::Command::new(cmd_path)
-                        .stdout(process::Stdio::piped())
-                        .stderr(process::Stdio::piped())
-                        .arg(args)
-                        .output()
-                        .unwrap();
-                    print!("{}", String::from_utf8_lossy(&output.stdout));
-                } else {
-                    println!("{}: command not found", cmd);
-                }
-            }
-        }
+        let command = Command::parse(cmd, args, command_registry.registry);
+        command.execute()
     }
 }
